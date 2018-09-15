3b64dc47ecba9ec0bba62ce7f54014f4e233a162
Abimbola Olaitan <craftwordltd@gmail.com>
2018-09-15 16:35:18 -0700

    modifies models

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    //mass assignment
+     protected $fillable = ['user_id', 'title', 'description','author', 'dateOfPublication'];
+     
+     public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
+
+     public function ratings()
+    {
+      return $this->hasMany(Rating::class);
+    }
 }
